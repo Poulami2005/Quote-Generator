674348d0048c043922262402941d656f69356522
Poulami2005 <poulami.ghosh7121@gmail.com>
2025-12-25 21:14:35 +0530

    Add random quote generator in quotes.py

---
+++ quotes.py
@@ -0,0 +1,13 @@
+import random
+
+quotes = [
+    "Believe in yourself.",
+    "Small steps every day lead to big results.",
+    "Dreams don’t work unless you do.",
+    "Stay patient and trust your journey.",
+    "Hard work beats talent when talent doesn’t work hard.",
+    "You are capable of more than you think."
+]
+
+print("✨ Random Quote ✨")
+print(random.choice(quotes))
